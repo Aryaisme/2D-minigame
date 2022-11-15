4024958abe4c6c7e7aa470bd11b2eb80ee881558
Aryaisme <59549664+Aryaisme@users.noreply.github.com>
2022-11-15 09:56:36 -0800

    Update CameraFollow.cs

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -11,13 +11,20 @@ public class CameraFollow : MonoBehaviour
 
     private void Awake()
     {
+        //getting the camera object
+        
         var cam = GetComponent<Camera>();
+        
+        // setting the size of the camera to the size of the screen so it is dynamic
+        
         cam.orthographicSize = (Screen.height/ 2f) / scale;
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        // set it on the camera
         t = target.transform;
     }
 
@@ -26,6 +33,7 @@ public class CameraFollow : MonoBehaviour
     {
         if (target != null)
         {
+            //move the camera with the players
             transform.position = new Vector3(t.position.x, t.position.y, transform.position.z);
         }
     }
