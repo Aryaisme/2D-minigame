1748dd445369d55543fa8fd0ac64853a27303665
Aryaisme <59549664+Aryaisme@users.noreply.github.com>
2022-11-15 11:51:44 -0800

    Update Explode.cs

--- Explode.cs
+++ Explode.cs
@@ -9,19 +9,6 @@ public class Explode : MonoBehaviour
     public Debris debris;
     public int totalDebris = 10;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D target)
     {
         if (target.gameObject.tag == "Deadly")
