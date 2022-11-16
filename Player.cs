10b988b0ca6f274f897691b1cd24ab3886af1eb2
Aryaisme <59549664+Aryaisme@users.noreply.github.com>
2022-11-16 10:53:09 -0800

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -6,13 +6,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // speed of the player   
     public float speed = 150f;
+        
+    // The terminal velocity
     public Vector2 maxVelocity = new Vector2(60, 100);
+    
+    //speed when moving up
     public float jetSpeed = 200f;
+    
     public bool standing;
     public float standingThreshold = 4f;
     public float airSpeelMultiplier = 0.3f;
 
+    // a refrence to components on the player
     private Rigidbody2D body2D;
     private SpriteRenderer renderer2D;
     private PlayerController controller;
@@ -21,6 +28,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // getting the parts
         body2D = GetComponent<Rigidbody2D>();
         renderer2D = GetComponent<SpriteRenderer>();
         controller = GetComponent<PlayerController>();
@@ -30,9 +38,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // get the velocity of the player in positive numbers
         var absVelX = Mathf.Abs(body2D.velocity.x);
         var absVelY = Mathf.Abs(body2D.velocity.y);
-
+        
+        // check to see if the player is standing or not
         if (absVelY <= standingThreshold)
         {
             standing = true;
@@ -41,21 +51,27 @@ public class Player : MonoBehaviour
         {
             standing = false;
         }
-
+        
+        //set the force to 0
         var forceX = 0f;
         var forceY = 0f;
 
+        // if the player is moving
         if(controller.moving.x != 0)
         {
+            // if we are moving at less than max velocity
             if (absVelX < maxVelocity.x)
             {
+                // set the new speed
                 var newSpeed = speed * controller.moving.x;
 
                 forceX = standing ? newSpeed : (newSpeed * airSpeelMultiplier);
-
+                
+                //flipping the charater if they are moving in another direction
                 renderer2D.flipX = forceX < 0;
             }
-
+            
+            //set the animation
             animator.SetInteger("AnimState", 1);
         }
         else
@@ -64,7 +80,7 @@ public class Player : MonoBehaviour
         }
 
 
-
+        // repeat with y axis
         if (controller.moving.y > 0)
         {
             if (absVelY < maxVelocity.y)
@@ -77,7 +93,8 @@ public class Player : MonoBehaviour
         {
             animator.SetInteger("AnimState", 3);
         }
-
+        
+        // apply the force to move
         body2D.AddForce(new Vector2(forceX, forceY));
 
     }
